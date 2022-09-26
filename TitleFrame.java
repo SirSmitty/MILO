71c3ae11d2f00d6eecba8ca8c1e8e169298bc9c8
ethan smith <ethansmith.s@icloud.com>
2022-09-26 14:18:57 -0700

    changed main file name

---
+++ TitleFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+class TitleFrame extends JFrame {
+
+    public TitleFrame() {
+
+        setTitle("MILO");
+        setSize(1250, 1000);
+
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
---
+++ TitlePanel.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+class TitlePanel extends JPanel {
+
+    public TitlePanel() {
+        // SETS BACKGROUD OF PANEL TO GRAY AND SETS THE LAYOUT OF THE PANEL
+        setBackground(Color.GRAY);
+        setLayout(null);
+
+    }
+}
