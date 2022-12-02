86716121875f518d578321af5e2f8487a836be4f
Ethan Weese <weese002@csusm.edu>
2022-12-02 12:07:28 -0800

    formatting
    Co-authored-by: Edgar Carbajal <carbajale99@users.noreply.github.com>

--- Apiconnect.java
+++ Apiconnect.java
@@ -1,6 +1,5 @@
 import javax.swing.JPanel;
 import javax.xml.transform.URIResolver;
-
 import java.awt.*;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -30,49 +29,47 @@ public class Apiconnect extends Object {
         return createResponseString(url);
     }
 
-    public URL createURL(String foodItem){
+    public URL createURL(String foodItem) {
 
         String keyString = "&api_key=" + API_KEY;
-        try{
-            URL url = new URL("https://api.nal.usda.gov/fdc/v1/foods/search?query=" + foodItem + "&pageSize=1" + keyString);
+        try {
+            URL url = new URL(
+                    "https://api.nal.usda.gov/fdc/v1/foods/search?query=" + foodItem + "&pageSize=1" + keyString);
             return url;
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             System.out.println("URL is invalid");
             return null;
-        }     
-    } 
-    
-    
-    public String createResponseString(URL url){
-        try{
+        }
+    }
+
+    public String createResponseString(URL url) {
+        try {
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.setRequestMethod("GET");
-    
-                int responseCode = con.getResponseCode();
-                String readLine = null;
-    
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-    
-                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                    StringBuffer response = new StringBuffer();
-                    while ((readLine = in.readLine()) != null) {
-                        response.append(readLine);
-                        response.append(System.lineSeparator());
-                    }
-                    in.close();
-                    return response.toString();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+            String readLine = null;
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                StringBuffer response = new StringBuffer();
+                while ((readLine = in.readLine()) != null) {
+                    response.append(readLine);
+                    response.append(System.lineSeparator());
                 }
-                else{
-                    return "invalid r code";
-                }
-            
-        }catch(Exception e){
+                in.close();
+                return response.toString();
+            } else {
+                return "invalid r code";
+            }
+
+        } catch (Exception e) {
             return "invalid";
         }
     }
 
-    public FoodItem getFoodItem(String foodString){
+    public FoodItem getFoodItem(String foodString) {
 
         String responseString = connectToApi(foodString.toLowerCase());
 
@@ -83,22 +80,21 @@ public class Apiconnect extends Object {
         JSONObject foodItem = foodsArray.getJSONObject(0);
         JSONArray foodNutrients = foodItem.getJSONArray("foodNutrients");
         String packageWeighString = foodItem.getString("packageWeight");
-        packageWeighString = packageWeighString.substring(packageWeighString.lastIndexOf('/')+1, packageWeighString.lastIndexOf(" "));
-        //this is in grams
-        int packageWeight = Integer.parseInt(packageWeighString); 
+        packageWeighString = packageWeighString.substring(packageWeighString.lastIndexOf('/') + 1,
+                packageWeighString.lastIndexOf(" "));
+        // this is in grams
+        int packageWeight = Integer.parseInt(packageWeighString);
         int servingSize = foodItem.getInt("servingSize");
 
-        int gramsPerServing = packageWeight/servingSize;
+        int gramsPerServing = packageWeight / servingSize;
         food.setServingSize(gramsPerServing);
-
 
         String nutrient;
 
         for (int i = 0; i < foodNutrients.length(); i++) {
 
-    
             nutrient = foodNutrients.getJSONObject(i).getString("nutrientName");
-            switch(nutrient){
+            switch (nutrient) {
                 case "Protein":
                     food.setProtein(foodNutrients.getJSONObject(i).getInt("value"));
                     break;
