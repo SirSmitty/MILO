e56bb91a3f2c3084003ce56e3cfe64f22123f450
ethan smith <ethansmith.s@icloud.com>
2023-04-13 12:51:17 -0700

    created planner

--- WorkoutFB/WorkoutPlanner/ExtremelyHighWorkoutPlanner.java
+++ WorkoutFB/WorkoutPlanner/ExtremelyHighWorkoutPlanner.java
@@ -1,5 +1,18 @@
 package WorkoutFB.WorkoutPlanner;
 
-public class ExtremelyHighWorkoutPlanner implements WorkoutPlanner_IF{
-    
+import PersonManagement.Person;
+import WorkoutFB.Workout.ExtremelyHighWorkout;
+import WorkoutFB.Workout.Workout_IF;
+
+public class ExtremelyHighWorkoutPlanner implements WorkoutPlanner_IF {
+
+    private Person person;
+
+    public ExtremelyHighWorkoutPlanner(Person p) {
+        this.person = p;
+    }
+
+    public Workout_IF createWorkout() {
+        return new ExtremelyHighWorkout(this.person);
+    }
 }
